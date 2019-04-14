886170ef43a81239dfce060852400bc32f934baa
Kamil Breguła <kamil.bregula@polidea.com>
2019-04-14 00:00:00 +0000

    First commit

---
+++ src/commands/webserver.js
@@ -0,0 +1,23 @@
+import { Command, flags } from '@oclif/command';
+import start from '../webserver';
+
+class HelloCommand extends Command {
+  async run() {
+    const { flags: flagsCommand } = this.parse(HelloCommand);
+    const name = flagsCommand.name || 'world';
+    this.log(`hello ${name} from ./src/commands/webserver.js`);
+
+    await start();
+  }
+}
+
+HelloCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`;
+
+HelloCommand.flags = {
+  name: flags.string({ char: 'n', description: 'name to print' }),
+};
+
+export default HelloCommand;
