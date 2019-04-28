24054b423176076529c8ca3d561bd1eb20e15c2f
Kamil Breguła <kamil.bregula@polidea.com>
2019-04-28 08:34:54 +0200

    Add base webserver view

--- src/webserver/index.js
+++ src/webserver/index.js
@@ -2,7 +2,7 @@
 import app from './app';
 
 
-export default async function start(port = 3000) {
+export default async function start(port = 8000) {
   app.listen(port, () => {
     /* eslint no-console: "off" */
     console.log(`Running on port ${port}...`);
